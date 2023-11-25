eeec1c4eb12f077aaf2f0baed0c86093d101f404
ZhouYang <xinzhuxiansheng@126.com>
2023-11-26 01:06:17 +0800

    jsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ type }: { type: string }) {
+  const p = <p>欢迎学习 React 通用后台开发</p>
+  const isAdmin = false
+  const style = { color: 'red', fontSzie: 22 }
+  const name = <span style={style}>yzhou</span>
+  const list = ['tom', 'Jack', 'Lucy', 'Lily']
+  const handlerChange = (e: any) => {
+    console.log(e.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div className='App'>
+      {p}
+      <p>{isAdmin ? '您好：管理员' : <span>普通访客</span>}</p>
+      {name}
+      <p>用户列表: </p>
+      <p>
+        {list.map(item => {
+          return <span key={item}>{item}</span>
+        })}
+      </p>
+      <p>
+        <input type='text' onChange={handlerChange} />
+      </p>
+    </div>
   )
 }
 
