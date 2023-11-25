fcb6324d1e539a2d365590de0579b9830ae42820
ZhouYang <xinzhuxiansheng@126.com>
2023-11-26 04:42:55 +0800

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const userRef = useRef<HTMLInputElement>(null)
+  const [val, setVal] = useState('')
+  const handleClick = () => {
+    userRef.current?.focus()
+    setVal(userRef.current?.value || '')
+    console.log(userRef.current?.className)
+  }
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div className='App'>
+      <input type='text' ref={userRef}></input>
+      <button onClick={handleClick}>按钮</button>
+      <p>{val}</p>
+    </div>
   )
 }
 
