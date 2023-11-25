55d2bf8e2e8acf475a5c3f87c7cb5d6de0ca6b6c
ZhouYang <xinzhuxiansheng@126.com>
2023-11-26 02:54:22 +0800

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  console.log('update app...')
+  const [name, setName] = useState('yzhou')
+  const [user, setUser] = useState({ name: 'yzhou', age: 30 })
+  const [list, setList] = useState(['Tom', 'Jack'])
   const [count, setCount] = useState(0)
-
+  const handleUpdate = () => {
+    setName('React')
+  }
+  const handleUser = () => {
+    setUser({ name: 'React', age: 50 })
+    setUser({ ...user, age: 50 })
+  }
+  const handleList = () => {
+    setList([...list, 'Lily'])
+  }
+  const handleCount = () => {
+    // setCount(count + 1)
+    // setCount(count + 1)
+    // setCount(count + 1) // 异步,只会 + 1
+    setTimeout(() => {
+      setCount(count + 1)
+    })
+  }
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div className='App'>
+      <p>欢迎学习 React 通用后台课程</p>
+      <p>{name}</p>
+      <p>
+        <span>用户名称：{user.name}</span>
+        <span style={{ marginLeft: 10 }}>用户年龄：{user.age}</span>
+      </p>
+      <p>
+        {list.map(item => {
+          return (
+            <span key={item} style={{ marginLeft: 10 }}>
+              {item}
+            </span>
+          )
+        })}
+      </p>
+      <p>
+        <span>Count: {count}</span>
+      </p>
+      <p>
+        <button onClick={handleUpdate}>修改名称</button>
+        <button onClick={handleUser}>修改名称</button>
+        <button onClick={handleList}>修改数组</button>
+        <button onClick={handleCount}>修改Count</button>
+      </p>
+    </div>
   )
 }
 
