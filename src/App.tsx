a0ddb4b4d484d6b2c21a666b45708b7fdf2cd140
ZhouYang <xinzhuxiansheng@126.com>
2023-11-26 03:52:14 +0800

    memo,callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
-import { useState } from 'react'
+import { memo, useMemo, useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const total1 = () => {
+    console.log('total1...')
+    const list = [1, 3, 5, 7, 9]
+    return list.reduce((prev, current) => prev + current, 0)
+  }
+
+  const total2 = useMemo(() => {
+    console.log('total2...')
+    const list = [1, 3, 5, 7, 9]
+    return list.reduce((prev, current) => prev + current, 0)
+  }, [])
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  const handleChildClick = useCallback(() => {
+    console.log('子节点点击')
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div className='App'>
+      <p>欢迎学习 React 后台课程</p>
+      <p>
+        <span>Count值：{count}</span>
+        <button onClick={handleClick}>按钮</button>
+      </p>
+      <p>{total1()}</p>
+      <p>{total2}</p>
+      <Child onClick={handleChildClick} />
+    </div>
   )
 }
+
+const Child = memo(({ onClick }: any) => {
+  console.log('child...')
+  return (
+    <p>
+      我是子节点 <button onClick={onClick}>按钮</button>
+    </p>
+  )
+})
 
 export default App
